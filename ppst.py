4369a49fc20824fae1f3f2f16fb4adf6dd74fc23
vacutice <vacutice@outlook.com>
2022-03-06 15:35:40 +0800

    make the code easy to read

--- ppst.py
+++ ppst.py
@@ -3,66 +3,18 @@ from random import randint, uniform
 from utils import *
 import numpy as np
 
-wp_mean=np.zeros((20,20),dtype=np.float64)
-wp_sigma=np.zeros((20,20),dtype=np.float64)
+# wp_mean=np.zeros((20,20),dtype=np.float64)
+# wp_sigma=np.zeros((20,20),dtype=np.float64)
 
 
 
-# wp_range[i,j]={'low_bound':wp_mean[i,j]-wp_sigma[i,j],'high_bound':wp_mean[i,j]+wp_sigma[i,j]}
+def wp_static_filter(points,wp_range,ItC):
+    # points=np.array((number_of_points,20),dtype=float64)
+    # wp_range={'low_bound':wp_mean-wp_sigma,'high_bound':wp_mean+wp_sigma}
+    # ItC=index_to_check
+    # return: possible wrong points and their resigned classes
 
-# index_to_check = {
-#     '1':[1,5,9,10,11,14,15],
-#     '2':[3,11,13,14,15,17],
-#     '3':[1,3,5,9,11,13,15,17],
-#     '4':[1,4,5,6,9,11,13,14,15],
-#     '5':[1,5,13,14,15],
-#     '6':[11,13,15],
-#     '7':[3,6,9,11,15],
-#     '8':[3,6,11,15],
-#     '9':[9,11,17],
-#     '10':[9,11,17],
-#     '11':[9,11,15,17],
-#     '12':[9,11,15,17],
-#     '13':[13,14,15],
-#     '14':[13,15],
-#     '15':[11,13,14,15,16,17,18,19],
-#     '16':[15,16,17],
-#     '17':[11,15,17],
-#     '18':[13,15,16] 
-#     '19':[6,15,18],  
-# }
-
-### ignore #0 and #7
-index_to_check = [
-    [0],                        #0
-    [1,5,9,10,11,14,15],        #1
-    [3,11,13,14,15,17],         #2
-    [1,3,5,9,11,13,15,17],      #3
-    [1,4,5,6,9,11,13,14,15],    #4
-    [1,5,13,14,15],             #5
-    [11,13,15],                 #6
-    # [3,6,9,11,15],              #7
-    [7]                         #7
-    [3,6,11,15],                #8
-    [9,11,17],                  #9
-    [9,11,17],                  #10
-    [9,11,15,17],               #11
-    [9,11,15,17],               #12
-    [13,14,15],                 #13
-    [13,15],                    #14
-    [11,13,14,15,16,17,18,19],  #15
-    [15,16,17],                 #16
-    [11,15,17],                 #17
-    [13,15,16],                 #18  
-    [6,15,18]                   #19
-]
-
-
-def wp_static_filter(points,wp_range={'low_bound':wp_mean-wp_sigma,'high_bound':wp_mean+wp_sigma},ItC=index_to_check):
-    # points n*20
-    # return possible wrong points and their resigned classes
     wp=np.zeros((points.shape[0],21),dtype=np.int32)
-    # wp_range[i,j]['low_bound']
     # wp[0,0]=-1
     wp_idx=0
     for i in range(points.shape[0]):
@@ -90,9 +42,6 @@ if __name__=='__main__':
     wp_m=np.zeros((20,20),dtype=np.float64)
     wp_s=np.zeros((20,20),dtype=np.float64)
 
-    
-    # wp_range[i,j]={'low_bound':wp_mean[i,j]-wp_sigma[i,j],'high_bound':wp_mean[i,j]+wp_sigma[i,j]}
-
     # index_to_check = {
     #     '1':[1,5,9,10,11,14,15],
     #     '2':[3,11,13,14,15,17],
@@ -115,6 +64,7 @@ if __name__=='__main__':
     #     '19':[6,15,18],  
     # }
 
+    ### ignore #0 and #7
     itc = [
     [0],                        #0
     [1,5,9,10,11,14,15],        #1
@@ -144,7 +94,7 @@ if __name__=='__main__':
         for j in range(20):
             wp_m[i,j]=uniform(0.3,0.7)
             wp_s[i,j]=uniform(0,0.3)
-            if j==0 or j==7:
+            if j==0 or j==7:    ### ignore #0 and #7
                 wp_m[i,j]=wp_s[i,j]=0
     wp_m=soft_max(wp_m) # do the softmax to make the data right
     n=200
